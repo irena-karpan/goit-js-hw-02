dff656fa054706d82d23bae2680671fae008de8c
Irena Karpan <irenakarpan@gmail.com>
2026-05-19 15:08:58 +0200

    fix else bug

--- js/task-1.js
+++ js/task-1.js
@@ -4,8 +4,9 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
     let totalPrice = quantity * pricePerDroid;
     if (customerCredits < totalPrice) {
         return "Insufficient funds!";
-    } else 
+    } else {
         return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+    }
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
